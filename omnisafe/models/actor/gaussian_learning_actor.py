015812132f48ec01484c8a8e3b8b3edc3bf4766e
ruiyang sun <rockmagma02@gmail.com>
2023-01-02 07:05:03 +0800

    feat(model): GuassianlaerningActor
    change device method

--- omnisafe/models/actor/gaussian_learning_actor.py
+++ omnisafe/models/actor/gaussian_learning_actor.py
@@ -65,7 +65,8 @@ class GaussianLearningActor(Actor):
 
     def _distribution(self, obs):
         mean = self.net(obs)
-        return Normal(mean, self._std)
+        std = torch.clamp(self._std, min=1e-6)
+        return Normal(mean, std)
 
     def predict(self, obs, deterministic=False, need_log_prob=False):
         dist = self._distribution(obs)
@@ -74,13 +75,12 @@ class GaussianLearningActor(Actor):
         else:
             out = dist.sample()
 
-        action = torch.clamp(out, -1, 1)
-        action = self.act_min + (action + 1) * 0.5 * (self.act_max - self.act_min)
+        action = self.act_min + (out + 1) * 0.5 * (self.act_max - self.act_min)
 
         if need_log_prob:
             log_prob = dist.log_prob(out).sum(axis=-1)
-            return out, log_prob
-        return out
+            return action, log_prob
+        return action
 
     def forward(self, obs, act=None):
         dist = self._distribution(obs)
@@ -94,3 +94,9 @@ class GaussianLearningActor(Actor):
     def std(self):
         """Return the current std."""
         return self._std.mean().item()
+
+    def change_device(self, device):
+        """Change the device of the actor."""
+        self.to(device)
+        self.act_min = self.act_min.to(device)
+        self.act_max = self.act_max.to(device)
