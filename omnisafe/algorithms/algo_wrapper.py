52dc2d226eed52df2951fb0646d807965379c4a4
ruiyang sun <rockmagma02@gmail.com>
2023-01-02 03:34:28 +0800

    fix(algo_wrapper): support offline algo

--- omnisafe/algorithms/algo_wrapper.py
+++ omnisafe/algorithms/algo_wrapper.py
@@ -49,8 +49,8 @@ class AlgoWrapper:
         self.algo_type = ALGORITHM2TYPE.get(self.algo, None)
         if self.algo_type is None or self.algo_type == '':
             raise ValueError(f'{self.algo} is not supported!')
-        if self.algo_type in ['off-policy', 'model-based']:
-            assert self.parallel == 1, 'off-policy or model-based only support parallel==1!'
+        if self.algo_type in ['off-policy', 'model-based', 'offline']:
+            assert self.parallel == 1, f'{self.algo_type} only support parallel==1!'
 
     def learn(self):
         """Agent Learning"""
