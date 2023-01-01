391d489ff65a6d1f51b044e0e20c0d5ee2d883d3
ruiyang sun <rockmagma02@gmail.com>
2023-01-02 02:08:29 +0800

    feat(offline): dataset

---
+++ omnisafe/utils/offline_dataset.py
@@ -0,0 +1,110 @@
+# Copyright 2022 OmniSafe Team. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""A dataset class for offline training."""
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+# pylint: disable=too-many-instance-attributes
+
+
+class OfflineDataset(Dataset):
+    """A dataset class for offline training."""
+
+    def __init__(
+        self,
+        data_path: str,
+        device: str,
+        obs_standardization: bool = True,
+    ):
+        """Initialize the dataset.
+
+        Args:
+            data_path: The path to the data.
+            device: The device to load the data to.
+        """
+        print('Loading data from', data_path)
+
+        try:
+            data = np.load(data_path)
+        except FileNotFoundError as exception:
+            raise FileNotFoundError(f'File {data_path} not found.') from exception
+
+        obs = data['obs']
+        action = data['action']
+        reward = data['reward']
+        cost = data['cost']
+        done = data['done']
+        next_obs = data['next_obs']
+
+        self.obs = torch.from_numpy(obs).float().to(device)
+        self.action = torch.from_numpy(action).float().to(device)
+        self.reward = torch.from_numpy(reward).float().to(device)
+        self.cost = torch.from_numpy(cost).float().to(device)
+        self.done = torch.from_numpy(done).float().to(device)
+        self.next_obs = torch.from_numpy(next_obs).float().to(device)
+
+        self.length = self.obs.shape[0]
+
+        print('Data loaded.')
+
+        print('Standardizing data.')
+        if obs_standardization:
+            self.obs_mean = self.obs.mean(dim=0)
+            self.obs_std = self.obs.std(dim=0)
+            self.obs = (self.obs - self.obs_mean) / (self.obs_std + 1e-8)
+            self.next_obs = (self.next_obs - self.obs_mean) / (self.obs_std + 1e-8)
+        else:
+            self.obs_mean = torch.zeros_like(self.obs[0])
+            self.obs_std = torch.ones_like(self.obs[0])  # for the case of no standardization
+        print(
+            f'Data standardized, the mean of obs is {self.obs_mean},\n the std of obs is {self.obs_std},'
+        )
+
+    def __len__(self):
+        """Return the length of the dataset."""
+        return self.length
+
+    def __getitem__(self, idx):
+        """Return the data and label at the given index."""
+        obs = self.obs[idx]
+        action = self.action[idx]
+        reward = self.reward[idx]
+        cost = self.cost[idx]
+        done = self.done[idx]
+        next_obs = self.next_obs[idx]
+
+        return obs, action, reward, cost, done, next_obs
+
+    def get_loader(self, batch_size: int, shuffle: bool = True):
+        """Return a data loader for the dataset.
+
+        Args:
+            batch_size: The batch size for the data loader.
+            shuffle: Whether to shuffle the dataset.
+
+        Returns:
+            A data loader for the dataset.
+        """
+        dataloader = DataLoader(self, batch_size=batch_size, shuffle=shuffle, drop_last=True)
+
+        def infinite_dataloader():
+            while True:
+                for data in dataloader:
+                    yield data
+
+        return infinite_dataloader()
